48ed523e6b0d223347b00ab9a820f39f932a33d6
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-03-16 15:26:06 -0300

    Adding Summary and TransactionsTable Components

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,11 +4,12 @@ export const GlobalStyle = createGlobalStyle`
 
   :root {
     --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
 
-    --text-tittle: #363F5F;
+    --text-title: #363F5F;
     --text-body: #969CB3;
 
     --background: #F0F2F5;
