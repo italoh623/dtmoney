1efe86ce351e10b5f937abcfbc7cbc170af3d788
Ítalo Henrique <ihls2@cin.ufpe.br>
2021-03-17 10:00:46 -0300

    Refactoring TransactionsContext to hook

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,11 @@
-import { FormEvent, useState, useContext } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
@@ -15,7 +15,7 @@ interface NewTransactionModalOpenProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequetClose }: NewTransactionModalOpenProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions()
 
   const [title, setTitle] = useState('');
   const [amount, setAmount] = useState(0);
@@ -36,7 +36,7 @@ export function NewTransactionModal({ isOpen, onRequetClose }: NewTransactionMod
     setAmount(0);
     setCategory('');
     setType('deposit');
-    
+
     onRequetClose();
   }
 
